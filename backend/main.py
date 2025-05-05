34c3efb8e6419f4dfe4f8d914f7532b839f2f60c
Resistenttt <121199607+Resistenttt@users.noreply.github.com>
2025-05-05 08:04:52 +0300

    main.py

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,9 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
 
 app = FastAPI()
-
-# Подключение статики
-app.mount("/static", StaticFiles(directory="/home/Resistent/clash/static"), name="static")
+app.mount("/static", StaticFiles(directory="../static"), name="static")
 
 @app.get("/")
-async def main():
-    return FileResponse("/home/Resistent/Clash/static/index.html")
-
-# Пример API
-@app.post("/open_case/{case_type}")
-async def open_case(case_type: str):
-    return {"item": "test_item", "case": case_type}
+def read_root():
+    return {"status": "OK"}
