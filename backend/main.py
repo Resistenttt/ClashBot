40866dd39c1d88352887cf39a2ecaeebf344a24e
Resistenttt <121199607+Resistenttt@users.noreply.github.com>
2025-05-05 08:35:54 +0300

    main.py

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,14 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from pathlib import Path
 
-# Укажите абсолютный путь
+# Сначала создаем приложение
+app = FastAPI()
+
+# Затем настраиваем статику
 static_path = Path(__file__).parent.parent / "static"
 app.mount("/static", StaticFiles(directory=str(static_path)), name="static")
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
