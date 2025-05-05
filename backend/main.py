be06c55c3709d9e9348668463ecd116d54fbc571
Resistenttt <121199607+Resistenttt@users.noreply.github.com>
2025-05-05 13:55:25 +0300

    main.py

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,14 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
-import random
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.post("/open_case/{case_type}")
-async def open_case(case_type: str, request: Request):
-    cases = {
-        "rusty": ["Glock-18 | Moonrise", "USP-S | Cortex"],
-        "tactical": ["AWP | Красная линия", "AK-47 | Огненный змей"]
-    }
-    return {"item": random.choice(cases[case_type])}
-
 @app.get("/")
-async def root():
+async def read_root():
     return FileResponse("static/index.html")
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
