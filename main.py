812f16ecf26c04c641e04eb346aeea01c4afcebf
Resistenttt <121199607+Resistenttt@users.noreply.github.com>
2025-05-06 14:07:03 +0300

    main.py

--- main.py
+++ main.py
@@ -15,13 +15,13 @@ CASES = [
         "key": "rusty",
         "title": "Rusty Case",
         "price": 500,
-        "img": "/assets/Case1.png",
+        "img": "/static/Case1.png",
         "items": [
-            {"name": "SG 553", "desc": "Army Sheen (Minimal Wear)", "img": "/assets/or1.png"},
-            {"name": "P250", "desc": "Boreal Forest (Battle-Scarred)", "img": "//assets/or2.png"},
-            {"name": "Five-SeveN", "desc": "Anodized Gunmetal (Factory New)", "img": "/assets/or3.png"},
-            {"name": "AWP", "desc": "Atheris (Field-Tested)", "img": "/assets/or4.png"},
-            {"name": "AK-47", "desc": "Redline (Minimal Wear)", "img": "/assets/or5.png"},
+            {"name": "SG 553", "desc": "Army Sheen (Minimal Wear)", "img": "/static/or1.png"},
+            {"name": "P250", "desc": "Boreal Forest (Battle-Scarred)", "img": "/static/or2.png"},
+            {"name": "Five-SeveN", "desc": "Anodized Gunmetal (Factory New)", "img": "/static/or3.png"},
+            {"name": "AWP", "desc": "Atheris (Field-Tested)", "img": "/static/or4.png"},
+            {"name": "AK-47", "desc": "Redline (Minimal Wear)", "img": "/static/or5.png"},
             {"name": "Desert Eagle", "desc": "Code Red (Factory New)", "img": "https://community.akamai.steamstatic.com/economy/image/1k3j2k3j2k3j2k3j2k3j2k3j2k3j2k3j2k3j2k3j2k3j2k/360fx360f"},
             {"name": "AWP", "desc": "Dragon Lore (Factory New)", "img": "https://community.akamai.steamstatic.com/economy/image/0k3j2k3j2k3j2k3j2k3j2k3j2k3j2k3j2k3j2k3j2k3j2k/360fx360f"},
         ]
@@ -30,7 +30,7 @@ CASES = [
         "key": "tactical",
         "title": "Tactical Case",
         "price": 3000,
-        "img": "/assets/Case2.png",
+        "img": "/static/Case2.pngg",
         "items": [
             {"name": "M4A1-S", "desc": "Guardian (Minimal Wear)", "img": "/assets/or6.png"},
             {"name": "USP-S", "desc": "Orion (Factory New)", "img": "/assets/or7.png"},
